a021506edde4639dc2429b0cac7577a0793f2a82
Thomas Wagenaar <t.wagenaar@student.tue.nl>
2022-05-20 19:28:29 +0200

    Okay-ish results. Stuff is weird

--- evaluation.py
+++ evaluation.py
@@ -3,23 +3,75 @@ from settings import *
 import itertools
 
 def population_action (population,inputs):
-    w1 = population[:,:n_hidden*n_inputs].reshape((population.shape[0],inputs.shape[2], n_hidden))
+    w1 = population[:,:n_hidden*n_inputs].reshape((population.shape[0],n_inputs, n_hidden))
     b1 = population[:,n_hidden*n_inputs:n_hidden*(n_inputs+1)].reshape((population.shape[0], 1, n_hidden))
     w2 = population[:,n_hidden*(n_inputs+1):n_hidden*(n_inputs+1+n_outputs)].reshape((population.shape[0],n_hidden, n_outputs))
     b2 = population[:,-n_outputs:].reshape((population.shape[0], 1, n_outputs))
 
+    # print(population[0])
+    # print(w1[0])
+    # print(b1[0])
+    # print(w2[0])
+    # print(b2[0])
 
     #print(inputs.shape, w1.shape, np.einsum('ijk,ikp->ijp', inputs,w1).shape)
     #print(inputs[0].dot(w1[0]))
     #print(np.einsum('ijk,ikp->ijp', inputs,w1)[0])
 
     z1 = np.einsum('ijk,ikp->ijp', inputs,w1) + b1
+
+    #print(w1.shape,inputs.shape,b1.shape)
+    #print(np.tanh(np.dot(np.tanh(inputs[0][0].dot(w1[0])+b1[0]), w2[0]) + b2[0]))
+    #print(z2[0][0])
     a1 = np.tanh(z1)
 
     z2 = np.einsum('ijk,ikp->ijp', a1,w2) + b2
-    a2 = np.tanh(z2)
+    a2 = np.sign(z2)
+
 
     return a2
+
+def fixed_action (population, inputs):
+    permutations = list(itertools.permutations(range(n_agents),n_agents))
+    formations = np.concatenate((np.reshape(inputs[:,:,4:], (population.shape[0], 3, 2, 2)), np.zeros((population.shape[0], 3, 1, 2))),axis=2)
+    formations -= np.reshape(np.mean(formations,axis=2), (population.shape[0], 3, 1, 2))
+    #print(formations.shape)
+
+    best = np.inf
+    best_order = None
+
+    relative_positions = np.reshape(inputs[:,:,:4], (population.shape[0], 3, 2, 2))
+
+    best_diff = np.ones((population.shape[0],3)) * np.inf
+    best_order_index = np.zeros((population.shape[0],3))
+    for i in range(len(permutations)):
+        order = permutations[i]
+        # permute formation, and see for which relative distances is most similar.
+        formation_copy = formations[:,:,list(order),:].copy()
+        formation_copy -= formation_copy[:,:,[0],:]
+
+        rel_dist_diff = np.mean(np.linalg.norm(relative_positions - formation_copy[:,:,1:],axis=3),axis=2)
+
+        best_order_index = np.where(rel_dist_diff < best_diff, np.ones((population.shape[0],3)) * i, best_order_index)
+        best_diff = np.where(rel_dist_diff < best_diff, rel_dist_diff, best_diff)
+
+    
+    best_orders = np.asarray(permutations)[best_order_index.astype('int')]
+
+    formation_copy = np.zeros((population.shape[0], 3, 3, 2))
+
+    for i in range(population.shape[0]):
+        for j in range(3):
+            formation_copy[i,j] = formations[i,j,best_orders[i,j]]
+    formation_copy -= formation_copy[:,:,[0],:]
+
+    #print(i, best_order, best, relative_positions, formation_copy[1:])
+
+    rel_dist_diff = np.sum(relative_positions - formation_copy[:,:,1:],axis=2)
+
+    rel_dist_diff = np.sign(rel_dist_diff)
+
+    return rel_dist_diff
 
 
         
@@ -59,21 +111,22 @@ def single_evaluate(population, save=False):
         np.save('data/init_pos.npy', initial_position)
         position_history = np.zeros((n_steps, population.shape[0], n_agents, 2))
 
+    ones = np.ones((population.shape[0],1,1))
     for i in range(n_steps):
         # Gather inputs for 1st agent
         inputs_0 = positions[:,[1,2],:] - positions[:,[0],:]
         inputs_0 = np.reshape(inputs_0, (population.shape[0],1,4))
-        inputs_0 = np.concatenate((inputs_0, formation_input), axis=2)
+        inputs_0 = np.concatenate((inputs_0, formation_input, ones* 0), axis=2)
 
         # Gather inputs for 2nd agent
         inputs_1 = positions[:,[0,2],:] - positions[:,[1],:]
         inputs_1 = np.reshape(inputs_1, (population.shape[0],1,4))
-        inputs_1 = np.concatenate((inputs_1, formation_input), axis=2)
+        inputs_1 = np.concatenate((inputs_1, formation_input, ones*2), axis=2)
 
         # Gather inputs for 3nd agent
         inputs_2 = positions[:,[0,1],:] - positions[:,[2],:]
         inputs_2 = np.reshape(inputs_2, (population.shape[0],1,4))
-        inputs_2 = np.concatenate((inputs_2, formation_input), axis=2)
+        inputs_2 = np.concatenate((inputs_2, formation_input, ones*3), axis=2)
 
         # Concenate to 3 samples per genome
         inputs = np.concatenate((inputs_0,inputs_1,inputs_2),axis=1)
--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ n_steps = 200     # Number of movement steps to simulate
 n_genomes = 100
 n_evals = 1
 
-n_hidden = 12
-n_inputs = 8
+n_hidden = 256
+n_inputs = 9
 n_outputs = 2
 
 #             weights + biases + weights + biases 
@@ -14,4 +14,4 @@ n_param = n_hidden*(n_inputs) + n_hidden + n_hidden * n_outputs + n_outputs
 print(n_param)
 
 p_c = 0.5 # Crossover probability
-p_m = 0.1 # Mutation probability
+p_m = 0.01 # Mutation probability
