23a0f1dff3ff561972ba7c9a88e4738c2a8ffff0
Thomas Wagenaar <t.wagenaar@student.tue.nl>
2022-06-03 08:39:34 +0200

    Add and plot results

--- analysis/fitness.py
+++ analysis/fitness.py
@@ -11,26 +11,31 @@ import settings
 fig, ax = plt.subplots(1)
 
 
-file_name = 'f_' + settings.to_str() + '.dat'
+# file_name = 'f_' + settings.to_str() + '.dat'
 folder = './runs/'
 
-run_settings = settings.from_file(file_name)
+file_names = os.listdir(folder)
+
+for file_name in file_names:
+    if file_name[0] != 'f': continue
+    run_settings = settings.from_file(file_name)
 
 
-fitnesses = np.fromfile(folder + file_name)
-epochs = np.arange(1,fitnesses.shape[0]+1)
+    fitnesses = np.fromfile(folder + file_name)
+    epochs = np.arange(1,fitnesses.shape[0]+1)
 
 
 
-''' Fitness history '''
-n_windows = 20 # must be even
-fitnesses = np.convolve(fitnesses, np.ones(n_windows) / n_windows, mode='valid')
+    ''' Fitness history '''
+    n_windows = 20 # must be even
+    fitnesses = np.convolve(fitnesses, np.ones(n_windows) / n_windows, mode='valid')
 
 
-if n_windows != 1:
-    ax.plot(epochs[int(n_windows/2)-1:-int(n_windows/2)],-fitnesses,label=str(run_settings['n_hidden']))
-else:
-    ax.plot(epochs,-fitnesses,label=str(run_settings['n_hidden']))
+    if n_windows != 1:
+        ax.plot(epochs[int(n_windows/2)-1:-int(n_windows/2)],-fitnesses,label=file_name)
+    else:
+        ax.plot(epochs,-fitnesses,label=file_name)
+
 
 ax.set_xlabel('Epoch')
 ax.set_ylabel('Fitness')
