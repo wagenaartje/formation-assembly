5d83cce312c2839157166a58722ef2c5423d15f9
Thomas Wagenaar <t.wagenaar@student.tue.nl>
2022-06-08 14:42:21 +0200

    Train scripts folder

--- train_scripts/train_base.py
+++ train_scripts/train_base.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('.')
+
 from train import train
 import json5
 
@@ -5,4 +8,5 @@ import json5
 with open('base_config.json') as f:
     base_config = json5.load(f)
 
-train(base_config)
+for i in range(10):
+    train(base_config)
